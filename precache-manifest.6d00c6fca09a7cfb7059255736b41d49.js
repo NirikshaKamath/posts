8324a38198110374d3e3219c576850338667ce9f
NirikshaSKamath <59636330+NirikshaSKamath@users.noreply.github.com>
2020-07-05 21:08:20 +0530

    Updates

---
+++ precache-manifest.6d00c6fca09a7cfb7059255736b41d49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a0c75eede3e61a7b16748bf8e41694",
+    "url": "/posts/index.html"
+  },
+  {
+    "revision": "b99f649794129ad942e1",
+    "url": "/posts/static/css/main.5e2d5ebe.chunk.css"
+  },
+  {
+    "revision": "b96774ff52ffa1e30a0d",
+    "url": "/posts/static/js/2.c7e54ccc.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/posts/static/js/2.c7e54ccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b99f649794129ad942e1",
+    "url": "/posts/static/js/main.29242e36.chunk.js"
+  },
+  {
+    "revision": "46155720457fa1a75bd0",
+    "url": "/posts/static/js/runtime-main.84989246.js"
+  },
+  {
+    "revision": "28776c86b15c7b1d326d3db841d3829c",
+    "url": "/posts/static/media/cached-black-18dp.28776c86.svg"
+  },
+  {
+    "revision": "19142cc51b8c074217df3d7499267bf0",
+    "url": "/posts/static/media/search-black-18dp.19142cc5.svg"
+  }
+]);
